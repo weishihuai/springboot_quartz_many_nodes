2456cb62571978fd420a132aa46509b8fe20d8d4
weishihuai <12345678>
2018-08-24 17:46:19 +0800

    基于SpringBoot & Quartz完成定时任务分布式多节点负载持久化

---
+++ src/main/java/com/springboot/wsh/SpringbootQuartzManyNodesApplication.java
@@ -0,0 +1,17 @@
+package com.springboot.wsh;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuartzManyNodesApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(SpringbootQuartzManyNodesApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootQuartzManyNodesApplication.class, args);
+		logger.info("【【【【【【定时任务分布式节点 - quartz-cluster-node-second 已启动】】】】】】");
+	}
+}
